da479ac8095f30254e9d5d65cce69a8a1c28304e
Vsux <vsux17@gmail.com>
2022-03-14 15:28:09 -0200

    coordinator

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,5 +1,3 @@
 
-
-
-export const BASE_URL = ''
+export const BASE_URL = 'https://us-central1-missao-newton.cloudfunctions.net'
 
--- src/pages/signUpPage/signUpPage.js
+++ src/pages/signUpPage/signUpPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const singUpPage = ()=>{
+export const signUpPage = ()=>{
     return(
 
         <div>singUpPage</div>
