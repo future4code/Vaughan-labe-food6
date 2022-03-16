edfc39dea41386a13c54ce6120af7f92525cbde7
ViniciusBarbosa <92874088+viniciuscicone@users.noreply.github.com>
2022-03-16 07:35:33 -0300

    update login

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,61 @@
+import { Button, TextField } from "@material-ui/core";
 import React from "react";
+import { InputContainer, LogoImage, ScreenContainer } from "./Styled";
+import logo1 from "../../assets/logo1.png"
+import { useForm } from "../../hook/useForm";
 
-export const LoginPage = ()=>{
-    return(
+export const LoginPage = () => {
+    const [form, onChange] = useForm({ name: "", email: ""})
 
-        <div>LoginPage</div>
+    
+    return (
+
+        <ScreenContainer>
+            <InputContainer>
+                <h2>Entrar</h2>
+                <LogoImage src={logo1} />
+                <TextField
+                    placeholder="Nome"
+                    name={"name"}
+                    onChange={onChange}
+                    label={"Nome"}
+                    variant={'outlined'}
+                    value={form.name}
+                    fullWidth
+                    required
+                    autoFocus
+                    margin="dense"
+
+                >
+
+                </TextField>
+
+
+                <TextField
+                    placeholder="Email"
+                    name={"email"}
+                    onChange={onChange}
+                    label={"E-mail"}
+                    variant={'outlined'}
+                    value={form.email}
+                    fullWidth
+                    required
+                    autoFocus
+                    margin="dense"
+
+                ></TextField>
+
+
+                <Button
+                    variant={'contained'}
+                    onClick={() => alert("Usuario logou")}
+                    fullWidth
+                    color={'default'}
+                >
+                    Entrar</Button>
+                    Nao tem uma conta ? <a href="/signup">Cadastre-se</a>
+            </InputContainer>
+        </ScreenContainer>
+
     )
 }
