740d7329279087f7c1ef9c70455dbd9cc97d9da5
IIVader <47644831+IIVader@users.noreply.github.com>
2022-03-15 22:27:44 -0300

    subindo os inputs do form e botão para fazer login

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,8 +1,60 @@
+import { Button, TextField } from "@material-ui/core";
 import React from "react";
+import { InputContainer, LogoImage, ScreenContainer } from "./Styled";
+import logo1 from "../../assets/logo1.png";
+import { useForm } from "../../hook/useForm";
 
-export const LoginPage = ()=>{
-    return(
+export const LoginPage = () => {
+  const [form, onChange] = useForm({ email: "", password: "" });
 
-        <div>LoginPage</div>
-    )
-}
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <ScreenContainer>
+      <InputContainer>
+        <LogoImage src={logo1} />
+        <form onSubmit={onSubmit}>
+          <TextField
+            placeholder="Email"
+            name={"email"}
+            onChange={onChange}
+            label={"email"}
+            variant={"outlined"}
+            value={form.email}
+            fullWidth
+            required
+            autoFocus
+            margin="dense"
+          />
+
+          <TextField
+            placeholder="Senha"
+            name={"password"}
+            onChange={onChange}
+            label={"Senha"}
+            variant={"outlined"}
+            value={form.password}
+            fullWidth
+            required
+            autoFocus
+            margin="dense"
+            id="senha"
+            type={"password"}
+          />
+
+          <Button
+            variant={"contained"}
+            type={"submit"}
+            fullWidth
+            color={"default"}
+          >
+            Criar
+          </Button>
+        </form>
+      </InputContainer>
+    </ScreenContainer>
+  );
+};
